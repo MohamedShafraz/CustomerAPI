9ac060dad838594e8d44405ac3b863d11bdd2709
MohamedShafraz <mshafraz0112@gmail.com>
2024-09-21 17:26:03 +0530

    Customer Model updated

--- src/main/java/com/firstSpring/Main.java
+++ src/main/java/com/firstSpring/Main.java
@@ -15,4 +15,11 @@ public class Main {
     public String home(){
         return "<center><h1>API Initiated Successfully</h1></center>";
     }
+//    //For Json  output
+//    @GetMapping("/")
+//    public HomeResponse home(){
+//        return new HomeResponse("<center><h1>API Initiated Successfully</h1></center>");
+//    }
+//    //like final class but record classes have more constraints compared to regular final classes
+//    record HomeResponse(String message){}
 }
